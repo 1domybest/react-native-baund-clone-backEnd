7b94325c9dfe27a9bc6ef11b380129f708c90203
1domybest <72382663+1domybest@users.noreply.github.com>
2023-01-11 10:05:18 +0900

    임시 비밀번호 찾기 구현완료
    임시 비밀번호 찾기 구현완료

--- src/main/java/com/example/backend/service/CommonService.java
+++ src/main/java/com/example/backend/service/CommonService.java
@@ -1,16 +1,20 @@
 package com.example.backend.service;
 
 import com.example.backend.common.exception.CustomApiException;
+import com.example.backend.domain.User;
 import com.example.backend.dto.ResponseMap;
 import com.example.backend.dto.common.request.RequestEmailAuthCodeDto;
 import com.example.backend.dto.common.response.ResponseEmailAuthCodeDto;
+import com.example.backend.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -21,6 +25,9 @@ import java.util.Random;
 public class CommonService {
     private final JavaMailSender emailSender;
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    private final UserRepository userRepository;
 
     public static String getRandomString() {
         int leftLimit = 48; // numeral '0'
@@ -32,50 +39,70 @@ public class CommonService {
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-        return generatedString;
+        return generatedString + "!";
     }
+
+    public void sendEmail (String to, String title,String message, boolean html) {
+        try {
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(to); // 메일 수신자
+            mimeMessageHelper.setSubject(title); // 메일 제목
+            mimeMessageHelper.setText(message, html); // 메일 본문 내용, HTML 여부
+            emailSender.send(mimeMessage);
+        }  catch (MessagingException e) {
+            throw new CustomApiException(title + "전송에 실패하였습니다.\n잠시후 다시시도해주세요", HttpStatus.BAD_REQUEST);
+        }
+    }
+
     public ResponseMap sendSimpleMessage(RequestEmailAuthCodeDto requestEmailAuthCodeDto) {
-        MimeMessage mimeMessage = emailSender.createMimeMessage();
-        int emailAuthCode = (int) (Math.random() * 100000);
-        System.out.println(emailAuthCode);
-        try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            mimeMessageHelper.setTo(requestEmailAuthCodeDto.getTo()); // 메일 수신자
-            mimeMessageHelper.setSubject("보안인증"); // 메일 제목
-            mimeMessageHelper.setText("<html><head></head><body><h1>보안 인증 번호입니다.</h1><h3>[ "+ emailAuthCode +" ]</h3></body></html>", true); // 메일 본문 내용, HTML 여부
-            emailSender.send(mimeMessage);
+        int emailAuthCode = (int)(Math.random() * (99999 - 10000 + 1)) + 10000;
+        String title = "보안인증";
+        String message = "<html><head></head><body><h1>보안 인증 번호입니다.</h1><h3>[ "+ emailAuthCode + " ]</h3></body></html>";
+        boolean html = true;
 
-            ResponseEmailAuthCodeDto responseEmailAuthCodeDto = new ResponseEmailAuthCodeDto();
-            responseEmailAuthCodeDto.setEmailAuthCode(String.valueOf(emailAuthCode));
-            ResponseMap responseMap = new ResponseMap();
-            responseMap.setHttpStatus(HttpStatus.OK);
-            responseMap.setMessage("인증번호가 전송되었습니다.");
-            responseMap.setData(new ResponseEntity<>(responseEmailAuthCodeDto, null, HttpStatus.OK));
-            return responseMap;
-        } catch (MessagingException e) {
-            throw new CustomApiException("인증번호 전송에 실패하였습니다.\n잠시후 다시시도해주세요", HttpStatus.BAD_REQUEST);
-        }
+        sendEmail(requestEmailAuthCodeDto.getTo(),title , message, html); // 이메일 전송 모듈
+
+        ResponseEmailAuthCodeDto responseEmailAuthCodeDto = new ResponseEmailAuthCodeDto();
+        responseEmailAuthCodeDto.setEmailAuthCode(String.valueOf(emailAuthCode));
+
+        ResponseMap responseMap = new ResponseMap();
+        responseMap.setHttpStatus(HttpStatus.OK);
+        responseMap.setMessage("인증번호가 전송되었습니다.");
+        responseMap.setData(new ResponseEntity<>(responseEmailAuthCodeDto, null, HttpStatus.OK));
+
+        return responseMap;
     }
 
-    public ResponseMap temporaryPassword(RequestEmailAuthCodeDto requestEmailAuthCodeDto) {
-        MimeMessage mimeMessage = emailSender.createMimeMessage();
+    @Transactional(readOnly = false)
+    public ResponseMap temporaryPassword(RequestEmailAuthCodeDto requestEmailAuthCodeDto) { // 비밀번호 암호화해서 교체해야함
         String getRandomString = getRandomString();
-        try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            mimeMessageHelper.setTo(requestEmailAuthCodeDto.getTo()); // 메일 수신자
-            mimeMessageHelper.setSubject("보안인증"); // 메일 제목
-            mimeMessageHelper.setText("<html><head></head><body><h1>보안 인증 번호입니다.</h1><h3>[ "+ getRandomString +" ]</h3></body></html>", true); // 메일 본문 내용, HTML 여부
-            emailSender.send(mimeMessage);
+        User user = userRepository.findByEmail(requestEmailAuthCodeDto.getTo());
 
-            ResponseEmailAuthCodeDto responseEmailAuthCodeDto = new ResponseEmailAuthCodeDto();
-            responseEmailAuthCodeDto.setEmailAuthCode(getRandomString);
-            ResponseMap responseMap = new ResponseMap();
-            responseMap.setHttpStatus(HttpStatus.BAD_REQUEST);
-            responseMap.setMessage("임시 비밀번호가 발급되었습니다.");
-            responseMap.setData(new ResponseEntity<>(responseEmailAuthCodeDto, null, HttpStatus.BAD_REQUEST));
-            return responseMap;
-        } catch (MessagingException e) {
-            throw new CustomApiException("임시비밀번호 발급에 실패하였습니다.\n잠시후 다시시도해주세요", HttpStatus.BAD_REQUEST);
+        if (user == null) { // 없는 게정일 경우
+            throw new CustomApiException("존재하지않는 계정입니다.\n다시한번 확인해주세요", HttpStatus.BAD_REQUEST);
         }
+
+        if (user.getPassword() == null && user.getProvider() != null) { // 비밀번호가 존재하지않고 sns로 가입하였다면.
+            throw new CustomApiException( user.getProvider() + "로 가입한 이력이있습니다. 로그인을 시도해주세요", HttpStatus.BAD_REQUEST);
+        }
+
+        String title = "임시비밀번호 발급";
+        String message = "<html><head></head><body><h1>보안 인증 번호입니다.</h1><h3>[ "+ getRandomString +" ]</h3></body></html>";
+        boolean html = true;
+        sendEmail(requestEmailAuthCodeDto.getTo(),title , message, html); // 이메일 전송 모듈
+
+        ResponseEmailAuthCodeDto responseEmailAuthCodeDto = new ResponseEmailAuthCodeDto();
+        responseEmailAuthCodeDto.setEmailAuthCode(getRandomString);
+
+        ResponseMap responseMap = new ResponseMap();
+        responseMap.setHttpStatus(HttpStatus.OK);
+        responseMap.setMessage("임시 비밀번호가 발급되었습니다.");
+        responseMap.setData(new ResponseEntity<>(responseEmailAuthCodeDto, null, HttpStatus.OK));
+
+        String encPassword = bCryptPasswordEncoder.encode(getRandomString); // 비밀번호 암호화
+        user.setPassword(encPassword); // 비밀번호 임시비밀번호로 변경
+
+        return responseMap;
     }
 }
